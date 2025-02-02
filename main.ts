09175687bac2f4b26c58727d547f4a0d6e3390bf
Александр Макаров <alexmk.work@gmail.com>
2025-02-02 05:32:58 +0500

    первый рабочий пример скачивания потока

--- main.ts
+++ main.ts
@@ -1,1 +1,34 @@
-import { Readable } from "stream";
+import { firefox } from "playwright";
+import { writeFileSync } from "node:fs";
+
+const browser = await firefox.launch();
+const page = await browser.newPage();
+
+let counter = 0;
+
+page.on("response", async (response) => {
+    const url = response.url();
+    const method = response.request().method();
+    if (method === "GET" && url.endsWith(".ts")) {
+        try {
+            const buffer = await response.body();
+            const timestamp = Date.now();
+            writeFileSync(`./output/segment_${timestamp}.ts`, buffer);
+
+            // writeFileSync(`./output/segment_out.txt`, String(counter++), {
+            //     flag: "a",
+            // });
+        } catch (e) {
+            if (e instanceof Error && e.message.includes("preflight")) {
+                console.warn("Preflight request error, skipping...");
+            } else {
+                throw e;
+            }
+        }
+    }
+});
+
+await page.goto("https://twitch.tv/hyver");
+await page.waitForTimeout(10000); // Playwright's built-in timeout
+
+await browser.close();
