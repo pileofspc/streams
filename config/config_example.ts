e59b4088484b9ce208f0664c8ae61b36e8ad7f05
pileofspc <alexmk.work@gmail.com>
2025-05-30 22:08:35 +0500

    changed config example to accomodate docker secrets

--- config/config_example.ts
+++ config/config_example.ts
@@ -4,4 +4,9 @@ export default {
     streamUrl: "https://twitch.tv/twitch",
     twitchWebhookExternalUrl: "https://example.com/stream",
     twitchWebhookInternalPort: 4000,
+    // for Docker Secrets:
+    secretsYoutubeTokens: "/run/secrets/tokens",
+    secretsYoutube: "/run/secrets/youtube_secret",
+    secretsTwitch: "/run/secrets/twitch_secret",
+    secretsTwitchAppId: "/run/secrets/twitch_app_id",
 } satisfies ConfigUser;
